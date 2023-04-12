f5e00c433ba53addf32d4d7a13e1e9bf71be4e20
DanielNewWorld <daniel.new.world@gmail.com>
2023-04-12 11:00:28 +0300

    12.04.23 commit

---
+++ app/src/main/java/com/example/braintrainers/trainers/TrainersActivity.java
@@ -0,0 +1,17 @@
+package com.example.braintrainers.trainers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.braintrainers.R;
+
+public class TrainersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainers);
+
+
+    }
+}
--- app/src/main/java/com/example/braintrainers/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/braintrainers/ui/home/HomeFragment.java
@@ -1,15 +1,19 @@
 package com.example.braintrainers.ui.home;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.braintrainers.DescriptionTrainers;
 import com.example.braintrainers.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -26,6 +30,20 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //main layout
+        Button btnHomeStart;
+        btnHomeStart = binding.btnHomeStart;
+        View.OnClickListener onClickGo = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, DescriptionTrainers.class);
+                context.startActivity(intent);
+            }
+        };
+        btnHomeStart.setOnClickListener(onClickGo);
+
         return root;
     }
 
