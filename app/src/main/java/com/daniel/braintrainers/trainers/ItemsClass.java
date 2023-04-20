6c2d93e0850115cc36dc134a08de001412f24103
DanielNewWorld <daniel.new.world@gmail.com>
2023-04-20 15:16:29 +0300

    first commit

---
+++ app/src/main/java/com/daniel/braintrainers/trainers/ItemsClass.java
@@ -0,0 +1,17 @@
+package com.daniel.braintrainers.trainers;
+
+class ItemsClass {
+    String name;
+    int color;
+    int z;
+    boolean booleanWIN;
+    int rotation;
+
+    ItemsClass(String name, int color, int z, boolean booleanWIN, int rotation) {
+        this.name = name;
+        this.color = color;
+        this.z = z;
+        this.booleanWIN = booleanWIN;
+        this.rotation = rotation;
+    }
+}
---
+++ app/src/main/java/com/daniel/braintrainers/ui/home/HomeFragment.java
@@ -0,0 +1,55 @@
+package com.daniel.braintrainers.ui.home;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.daniel.braintrainers.DescriptionTrainers;
+import com.daniel.braintrainers.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //main layout
+        Button btnHomeStart;
+        btnHomeStart = binding.btnHomeStart;
+        View.OnClickListener onClickGo = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, DescriptionTrainers.class);
+                context.startActivity(intent);
+            }
+        };
+        btnHomeStart.setOnClickListener(onClickGo);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
